cfed9851bfa84a3cbdcc733d7341ea141db9a1a1
King <zaqrhkd2@naver.com>
2026-05-07 20:26:31 +0900

    🔍 modify: 카카오맵 지도 좌표 보정

--- lib/location.ts
+++ lib/location.ts
@@ -2,8 +2,8 @@ export const houseLocation = {
   name: "미림 규웅 House",
   placeName: "운암주공4단지 405동",
   address: "경기도 오산시 운암로 45 운암주공4단지 405동 1604호",
-  lat: 37.15125,
-  lng: 127.07725,
+  lat: 37.15078,
+  lng: 127.07547,
 };
 
 export const encodedPlaceName = encodeURIComponent(houseLocation.name);
